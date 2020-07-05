7e894f1cf5844f6342e0819cd5780455cf881dc2
minung.han <hmu332233@gmail.com>
2020-07-05 12:44:12 +0900

    build: add 'get' function for RequiredError

--- models/users/index.js
+++ models/users/index.js
@@ -3,3 +3,4 @@ const { db, CONSTANTS } = require('./db');
 exports.CONSTANTS = CONSTANTS;
 exports.db = db;
 exports.find = (...params) => db.find(...params).lean();
+exports.findOne = (...params) => db.findOne(...params).lean();
