1a92d5705084089fb1b31e132270856bb8c50602
minung.han <hmu332233@gmail.com>
2020-07-08 01:31:29 +0900

    refactor: add exec() to mongoose queries

--- models/users/index.js
+++ models/users/index.js
@@ -2,5 +2,6 @@ const { db, CONSTANTS } = require('./db');
 
 exports.CONSTANTS = CONSTANTS;
 exports.db = db;
-exports.find = (...params) => db.find(...params).lean();
-exports.findOne = (...params) => db.findOne(...params).lean();
+
+exports.find = (...params) => db.find(...params).lean().exec(); // prettier-ignore
+exports.findOne = (...params) => db.findOne(...params).lean().exec(); // prettier-ignore
